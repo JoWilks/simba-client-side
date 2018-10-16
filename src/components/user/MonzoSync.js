305a37468926c0c53f089c7ff6724f51b9bf3396
Flatiron School <flatiron@Flatirons-MacBook-Air.local>
2018-10-16 17:15:28 +0100

    changed redirect uri, Jo

--- src/components/user/MonzoSync.js
+++ src/components/user/MonzoSync.js
@@ -6,14 +6,14 @@ class MonzoSync extends React.Component {
     render () {
         //will need to put these in state, and potentially pull from server?
         const client_id = "oauth2client_00009bXcUYcbaBlM4oMMqX"
-        const redirect_uri = "https://zealous-kalam-8b6c52.netlify.com/dashboard" //will definitely need to change to an actual hosted domain
+        const redirect_uri = "https://zealous-kalam-8b6c52.netlify.com/" //will definitely need to change to an actual hosted domain
         const state_token = "randomstring" //need to hide this somehow
         const link = `https://auth.monzo.com/?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=code&state=${state_token}`
 
         return (
             <div>
                 <h1>Click to sync the app with your Monzo Account</h1>
-                <a href={link}>
+                <a href={link} >
                     <img src='https://www.vectorlogo.zone/logos/monzo/monzo-card.png' alt='monzo_redirect' />
                 </a>
             </div>
