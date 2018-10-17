d5acfd6f47e84cb653db33534f9323beb654f95d
Flatiron School <flatiron@Flatirons-MacBook-Air.local>
2018-10-17 17:10:46 +0100

    added some meters, and basic css styling budget page, Jo

--- src/components/budget/Budget.js
+++ src/components/budget/Budget.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import { moment } from '../../datefunctions'
 import BudgetSettings from './BudgetSettings'
+import { LineMeter, HalfCircleMeter } from 'react-svg-meters'
+import { moment } from '../../datefunctions'
+import './Budget.css' 
 
 const timeFrames = ['monthly', 'weekly', 'daily']
-
 
 class Budget extends React.Component {
     
@@ -19,18 +20,36 @@ class Budget extends React.Component {
         this.setState({ viewSettings: !this.state.viewSettings })
     } 
 
+    calculateBudget = (timeFrame) => {
+       const allDebits = JSON.parse(JSON.stringify(this.props.transactions.debits))
+
+    }
 
     render () {
         return (
             <div>
                     <h1>Budget View</h1>
+                    {/* should put button on toolbar */}
                     <button onClick={this.toggleViewSettings}>{this.state.viewSettings ? "Close Settings": "Open Budget Settings"}</button>
                     {
-                        this.state.viewSettings &&
-                        <BudgetSettings categories={this.props.categories.debit}/>
+                        this.state.viewSettings 
+                        ? <BudgetSettings categories={this.props.categories.debit}/>
+                        : (<div className=''>
+                            <p>Here's how your total {this.state.timeFrame} budget is tracking:</p> <br></br>
+                                <HalfCircleMeter value={75}/> <br></br>
+                                <p>How much you've spent on each category...</p> <br></br>
+                                <div className='container'>
+                                {this.props.categories.debit.map(category => 
+                                        <div className={`budget ${category}`}>
+                                            {category} <LineMeter   size={75}
+                                                                    value={75}/>
+                                        </div>
+                                )}
+                                </div>
+                            </div>
+                        )
                     }
 
-                    Here's how you budget is tracking for the 
             </div>
         )
     }
